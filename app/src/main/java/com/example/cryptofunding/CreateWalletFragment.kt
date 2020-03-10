5e0e67d8dffd6555553dfc371891803a3fe94cec
student5308 <12068940+LilCurly@users.noreply.github.com>
2020-03-10 15:03:08 +0100

    Added drawables to the left of each EditText with a selector

--- app/src/main/java/com/example/cryptofunding/CreateWalletFragment.kt
+++ app/src/main/java/com/example/cryptofunding/CreateWalletFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.cryptofunding.databinding.FragmentCreateWalletBinding
 import com.example.cryptofunding.di.injector
 import com.example.cryptofunding.viewmodel.viewModel
 import kotlinx.android.synthetic.main.fragment_create_wallet.*
+import kotlin.math.roundToInt
 
 /**
  * A simple [Fragment] subclass.
@@ -31,6 +33,8 @@ class CreateWalletFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val viewModel = (parentFragment as NewWalletFragment).viewModel
 
+        setupEditTextDrawables()
+
         binding.createwalletLogin.setOnFocusChangeListener { _, hasFocus ->
             if (!hasFocus) {
                 viewModel.validateName()
@@ -41,6 +45,23 @@ class CreateWalletFragment : Fragment() {
             if (!hasFocus) {
                 viewModel.validePassword()
             }
+        }
+    }
+
+    private fun setupEditTextDrawables() {
+        val density = resources.displayMetrics.density
+        val size = (20 * density).roundToInt()
+
+        val loginDrawable = ContextCompat.getDrawable(context!!, R.drawable.login_drawable_selector)
+        loginDrawable?.let {
+            it.setBounds(0, 0, size, size)
+            createwallet_login.setCompoundDrawables(it, null, null, null)
+        }
+
+        val passwordDrawable = ContextCompat.getDrawable(context!!, R.drawable.password_drawable_selector)
+        passwordDrawable?.let {
+            it.setBounds(0, 0, size, size)
+            createwallet_password.setCompoundDrawables(it, null, null, null)
         }
     }
 
--- app/src/main/java/com/example/cryptofunding/ImportWalletFragment.kt
+++ app/src/main/java/com/example/cryptofunding/ImportWalletFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.cryptofunding.databinding.FragmentCreateWalletBinding
 import com.example.cryptofunding.databinding.FragmentImportWalletBinding
 import com.example.cryptofunding.di.injector
 import com.example.cryptofunding.viewmodel.viewModel
+import kotlinx.android.synthetic.main.fragment_create_wallet.*
 import kotlinx.android.synthetic.main.fragment_import_wallet.*
+import kotlin.math.roundToInt
 
 class ImportWalletFragment : Fragment() {
     lateinit var binding: FragmentImportWalletBinding
@@ -29,6 +32,8 @@ class ImportWalletFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val viewModel = (parentFragment as NewWalletFragment).viewModel
 
+        setupEditTextDrawables()
+
         binding.importwalletLogin.setOnFocusChangeListener { _, hasFocus ->
             if (!hasFocus) {
                 viewModel.validateName()
@@ -45,6 +50,29 @@ class ImportWalletFragment : Fragment() {
             if (!hasFocus) {
                 viewModel.validatePrivateKey()
             }
+        }
+    }
+
+    private fun setupEditTextDrawables() {
+        val density = resources.displayMetrics.density
+        val size = (20 * density).roundToInt()
+
+        val loginDrawable = ContextCompat.getDrawable(context!!, R.drawable.login_drawable_selector)
+        loginDrawable?.let {
+            it.setBounds(0, 0, size, size)
+            importwallet_login.setCompoundDrawables(it, null, null, null)
+        }
+
+        val passwordDrawable = ContextCompat.getDrawable(context!!, R.drawable.password_drawable_selector)
+        passwordDrawable?.let {
+            it.setBounds(0, 0, size, size)
+            importwallet_password.setCompoundDrawables(it, null, null, null)
+        }
+
+        val keyDrawable = ContextCompat.getDrawable(context!!, R.drawable.key_drawable_selector)
+        keyDrawable?.let {
+            it.setBounds(0, 0, size, size)
+            importwallet_privatekey.setCompoundDrawables(it, null, null, null)
         }
     }
 }
