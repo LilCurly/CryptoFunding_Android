859fe10b2f3cce4d0a6dd6c8e66a87d82f69ba57
Roman Muzikantov <12068940+LilCurly@users.noreply.github.com>
2020-07-21 12:49:49 +0200

    Setting up the projet details layout

---
+++ app/src/main/java/com/example/cryptofunding/projectDetailFragment.kt
@@ -0,0 +1,34 @@
+package com.example.cryptofunding
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
+import kotlinx.android.synthetic.main.fragment_project_detail.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class projectDetailFragment : Fragment() {
+    lateinit var imageList: List<SlideModel>
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_project_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        imageList = listOf(SlideModel(R.drawable.avengers_poster), SlideModel(R.drawable.avengers_poster), SlideModel(R.drawable.avengers_poster))
+        imageSlider.setImageList(imageList, ScaleTypes.CENTER_CROP)
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
