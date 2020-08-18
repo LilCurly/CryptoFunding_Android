6c6f3e8b266a4b92646ede3b3d3afe9126eba974
student5308 <student5308@tfns5308.local>
2020-08-18 11:47:27 +0200

    adding a mapper from a Firestore snapshot to a Project model

---
+++ app/src/main/java/com/example/cryptofunding/data/mapper/ProjectMapper.kt
@@ -0,0 +1,22 @@
+package com.example.cryptofunding.data.mapper
+
+import com.example.cryptofunding.data.CategoryType
+import com.example.cryptofunding.data.Project
+import com.google.firebase.firestore.DocumentSnapshot
+
+class ProjectMapper {
+    companion object {
+        fun mapToProject(document: DocumentSnapshot): Project {
+            val title = document.getString("title")!!
+            val summary = document.getString("summary")!!
+            val category = document.getString("category")!!
+            val images = document.get("images") as List<String>
+            val categoryType = CategoryType.valueOf(category)
+
+            val project = Project(title, summary, categoryType)
+            project.imagesUrl = images
+
+            return project
+        }
+    }
+}
